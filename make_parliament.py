3f5e6f14471d49d5d5e5ff5b233c8e1249c381d7
Tye Shutty <shuttyt@gmail.com>
2021-09-29 16:29:37 -0300

    changes

--- make_parliament.py
+++ make_parliament.py
@@ -28,27 +28,34 @@ with open('2019 results by candidate.csv', "r", newline='') as csvfile:
 
 districts = {}
 party_names = ['Liberal', 'Conservative', 'NDP-New Democratic Party', 'Green Party', 'VCP', "People's Party", 'Christian Heritage Party',
-'Independent', 'National Citizens Alliance', 'Communist', 'Animal Protection Party', 'Libertarian']
-parties = {}
+'Independent', 'National Citizens Alliance', 'Communist', 'Animal Protection Party', 'Libertarian', 'Parti Rhinocéros Party', 'Bloc Québécois',
+'Radical Marijuana', "l'Indépendance du Québec", 'ML', 'No Affiliation', 'PC Party', 'Nationalist', "CFF - Canada's Fourth Front", 'UPC',
+'Stop Climate Change']
+party_candidate_vote = {}
+party_total_vote = {}
 total_rep_votes = 0
 total_unrep_votes = 0
+SEATS_IN_PARLIAMENT = 338
+party_winners = {}
 
 for candidate in candidates:
   (prov, district_name, district_num, name_and_party, residence, occupation, votes, vote_per, maj, maj_per) = candidate
   votes = int(votes)
 
   if district_num not in districts:
+    districts[district_num] = {}
     districts[district_num]['district_name'] = district_name
     districts[district_num] = {'candidate_votes': {name_and_party: votes}}
     districts[district_num]['total_votes'] = votes
     districts[district_num]['winner'] = name_and_party
+    districts[district_num]['runner_up'] = 'none'
   else:
     districts[district_num]['candidate_votes'][name_and_party] = votes
     districts[district_num]['total_votes'] += votes
     if votes > districts[district_num]['candidate_votes'][districts[district_num]['winner']]:
       districts[district_num]['runner_up'] = districts[district_num]['winner']
       districts[district_num]['winner'] = name_and_party
-    elif votes > districts[district_num]['candidate_votes'][districts[district_num]['runner_up']]:
+    elif districts[district_num]['runner_up'] == 'none' or votes > districts[district_num]['candidate_votes'][districts[district_num]['runner_up']]:
       districts[district_num]['runner_up'] = name_and_party
 
 
@@ -76,17 +83,37 @@ for candidate in candidates:
         raise Exception('no name in', name_and_party)
       else:
         name += f' ({district_name})'
-      if party not in parties:
-        parties[party] = {name: {'rep_votes': rep_votes, 'unrep_votes': unrep_votes}}
+      if party not in party_candidate_vote:
+        party_candidate_vote[party] = {name: {'rep_votes': rep_votes, 'unrep_votes': unrep_votes}}
+        party_total_vote[party] = {'rep_votes': rep_votes, 'unrep_votes': unrep_votes}
       else:
-        parties[party][name] = votes
+        party_candidate_vote[party][name] = {'rep_votes': rep_votes, 'unrep_votes': unrep_votes}
+        party_total_vote[party]['rep_votes'] += rep_votes
+        party_total_vote[party]['unrep_votes'] += unrep_votes
 
       break
   if not found:
     raise Exception('no party for',name_and_party)
 
+seats_in_unrep_parliament = round(SEATS_IN_PARLIAMENT*total_unrep_votes/total_rep_votes)
+print(seats_in_unrep_parliament, 'unrep seats')
 
+for party in party_names:
+  party_total_vote[party]['seats'] = round(seats_in_unrep_parliament*party_total_vote[party]['unrep_votes']/total_unrep_votes)
+  # print(party,'rep votes=',party_total_vote[party]['rep_votes'], 
+  # 'unrep votes=',party_total_vote[party]['unrep_votes'],
+  # 'unrep seats=',round(seats_in_unrep_parliament*party_total_vote[party]['unrep_votes']/total_unrep_votes))
 
+  # print(party_candidate_vote[party])
+  party_winners[party] = [(candidate, party_candidate_vote[party][candidate]['unrep_votes']) for candidate in list(party_candidate_vote[party])]
+  party_winners[party] = sorted(party_winners[party], key=lambda candidate: candidate[1]*-1)[:party_total_vote[party]['seats']]
+  # print(party,'winners:',party_winners[party])
 
+# new_tag = doc.new_tag('a',href='blah')
+# doc.find('p').append(new_tag)
+# doc.find('body').insert(0,new_tag)
+
+party_names.sort(key=lambda party: -len(party_winners[party]))
+# print(party_names)
 
 # print(doc.prettify())
